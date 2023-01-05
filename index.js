d9f9da045f6c79a41605003575113c6a42f4eb87
Illia Chernukha <ilyachernuha@gmail.com>
2023-01-05 19:14:41 +0200

    feat: add /api/users/:_id/exercises and /api/users/:_id/logs endpoints

--- index.js
+++ index.js
@@ -60,20 +60,20 @@ app.get("/", (req, res) => {
 });
 
 app.post("/api/users", async (req, res) => {
-  const newUser = new User({
-    username: req.body.username,
-  });
   try {
+    const newUser = new User({
+      username: req.body.username,
+    });
     await newUser.save();
+    return res.json({
+      _id: newUser._id,
+      username: newUser.username,
+    });
   } catch (error) {
     return res.json({
       error: error.message,
     });
   }
-  return res.json({
-    _id: newUser._id,
-    username: newUser.username,
-  });
 });
 
 app.get("/api/users", async (req, res) => {
@@ -81,13 +81,61 @@ app.get("/api/users", async (req, res) => {
     const users = await User.find({}, { _id: 1, username: 1 });
     return res.json(users);
   } catch (error) {
-    return res.json({ error: error.message });
+    return res.json({
+      error: error.message,
+    });
   }
 });
 
-app.post("/api/users/:_id/exercises", (req, res) => {});
+app.post("/api/users/:_id/exercises", async (req, res) => {
+  try {
+    let user = await User.findById(req.params._id);
+    if (!user) {
+      return res.json({error: "User not found"});
+    }
+    let exercise = new Exercise({
+      userId: user._id,
+      description: req.body.description,
+      duration: req.body.duration
+    });
+    const date = Date.parse(req.body.date);
+    if (!date) {
+      exercise.date = new Date(date);
+    }
+    user.exercises.push(exercise._id);
+    await user.save();
+    return res.json({
+      _id: user._id,
+      username: user.username,
+      description: exercise.description,
+      duration: exercise.duration,
+      date: new Date(exercise.date).toDateString()
+    });
+  } catch (error) {
+    return res.json({
+      error: error.message,
+    });
+  }
+});
 
-app.get("/api/users/:_id/logs", (req, res) => {});
+app.get("/api/users/:_id/logs", async (req, res) => {
+  try {
+    const user = await User.findById(req.params._id);
+    if (!user) {
+      return res.json({ error: "User not found" });
+    }
+    return res.json({
+      _id: user._id,
+      username: user.username,
+      count: user.exercises.length,
+      log: user.exercises,
+    });
+  } catch (error) {
+    return res.json({
+      error: error.message,
+    });
+  }
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log("Your app is listening on port " + listener.address().port);
