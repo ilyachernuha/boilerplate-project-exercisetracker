2351ff1d297a62f22db89e1c0ac09198bc72a3b1
Illia Chernukha <ilyachernuha@gmail.com>
2023-01-06 14:02:20 +0200

    fix: add mongo connection log

--- index.js
+++ index.js
@@ -7,8 +7,13 @@ const e = require("express");
 require("dotenv").config();
 
 mongoose.set("strictQuery", false);
-mongoose.connect(process.env.MONGO_URI).catch((error) => console.error(error));
-const connection = mongoose.connection;
+mongoose
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true })
+  .catch((error) => console.error(error));
+
+const db = mongoose.connection;
+db.on("error", error => console.error(error));
+db.once("open", () => console.log("Connected to MongoDB"));
 
 const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
